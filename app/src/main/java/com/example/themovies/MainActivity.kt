69da96af0501ece76a53c7a890118ce47c2c2d88
Dima <dmitry.turov374@gmail.com>
2021-03-04 17:42:33 +0300

    add movies fragment

--- app/src/main/java/com/example/themovies/MainActivity.kt
+++ app/src/main/java/com/example/themovies/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.themovies
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val fragmentManager = supportFragmentManager
+
+    private val moviesFragment = MoviesFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            fragmentManager.beginTransaction()
+                .replace(R.id.nav_host_container, moviesFragment)
+                .commit()
+        }
     }
 }
